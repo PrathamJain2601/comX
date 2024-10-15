94300d72d677502228ce8df2c024482a30e00603
Pratham Jain <jainpratham2601@gmail.com>
2024-10-15 14:37:15 +0530

    forgot password

--- comX-backend/src/controllers/auth-controller/send-forgot-password-otp.controller.ts
+++ comX-backend/src/controllers/auth-controller/send-forgot-password-otp.controller.ts
@@ -1,4 +1,16 @@
 import { Request, Response } from "express";
-export function send_forgot_password_otp(req: Request, res: Response){
-    
+import { responseCodes } from "../../utils/response-codes";
+import { generateOTP, sendOtpEmail } from "./send-email-otp.controller";
+import { prisma } from "../../config/dbConnect";
+export const send_forgot_password_otp = async(req: Request, res: Response) =>{
+    try {
+        const { email } = req.body;
+        const otp = generateOTP();
+        await sendOtpEmail(email, otp, 'Forgot Password OTP', `Your OTP for forgot Password is: ${otp}. It is valid for 10 minutes.`).then(() => console.log('OTP sent!'));
+        responseCodes.success.ok(res, {}, "otp sent");
+      }
+      catch (error) {
+        console.log(error);
+        responseCodes.serverError.internalServerError(res, "internal error");
+      }
 }
