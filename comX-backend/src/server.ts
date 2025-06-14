d63c3581c8c7243485f860b26904cf27169ad5ef
Pratham Jain <jainpratham2601@gmail.com>
2025-06-14 19:00:26 +0530

    server port done

--- comX-backend/src/server.ts
+++ comX-backend/src/server.ts
@@ -111,7 +111,7 @@ io.on('connect', (socket) => {
 });
 
 // ─── Start Server ───────────────────────────────────
-const PORT = parseInt(process.env.PORT || '3000', 10);
+const PORT = parseInt(process.env.PORT as string, 10);
 server.listen(PORT, () => {
   console.log(`Server + WebSocket running on port ${PORT}`);
 });
